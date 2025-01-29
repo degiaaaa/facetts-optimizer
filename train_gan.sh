eea0525d6a5f3aa62f0985f20ac2a2c6c0dfeedf
degiaaaa <debie1997@yahoo.de>
2025-01-29 22:02:56 +0100

    29.01

--- train_gan.sh
+++ train_gan.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -J train_gan
 #SBATCH --partition=2080-galvani  # Use the CPU-only partition
-#SBATCH --ntasks=4               # Number of tasks
-#SBATCH --gres=gpu:8
-#SBATCH --mem=100G                # Amount of memory
+#SBATCH --ntasks-per-node=1              # Number of tasks
+#SBATCH --gres=gpu:6
+#SBATCH --mem=80G                # Amount of memory
 #SBATCH --time=3-00:00:00       # Maximum runtime
 #SBATCH --output=train_gan-%j.out
 #SBATCH --error=train_gan-%j.err
@@ -17,4 +17,4 @@ source /mnt/qb/work2/butz1/bst080/miniconda3/etc/profile.d/conda.sh
 #conda activate label_env
 conda activate /mnt/qb/work2/butz1/bst080/miniconda3/envs/train_env  #/mnt/qb/home/butz/bst080/miniconda3/envs/label_env
 
-srun conda run -p /mnt/qb/work2/butz1/bst080/miniconda3/envs/train_env python /mnt/qb/work/butz/bst080/facetts-optimizer/run_gan.py
+srun conda run -p /mnt/qb/work2/butz1/bst080/miniconda3/envs/train_env python /mnt/qb/work/butz/bst080/faceGANtts/run_gan.py
--- train_orig.sh
+++ train_orig.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -J train_orig
 #SBATCH --partition=2080-galvani  # Use the CPU-only partition
-#SBATCH --ntasks=4               # Number of tasks
-#SBATCH --gres=gpu:8
-#SBATCH --mem=100G                # Amount of memory
+#SBATCH --ntasks-per-node=1               # Number of tasks
+#SBATCH --gres=gpu:6
+#SBATCH --mem=80G                # Amount of memory
 #SBATCH --time=3-00:00:00       # Maximum runtime
 #SBATCH --output=train_orig-%j.out
 #SBATCH --error=train_orig-%j.err
@@ -17,4 +17,4 @@ source /mnt/qb/work2/butz1/bst080/miniconda3/etc/profile.d/conda.sh
 #conda activate label_env
 conda activate /mnt/qb/work2/butz1/bst080/miniconda3/envs/train_env  #/mnt/qb/home/butz/bst080/miniconda3/envs/label_env
 
-srun conda run -p /mnt/qb/work2/butz1/bst080/miniconda3/envs/train_env python /mnt/qb/work/butz/bst080/facetts-optimizer/run_orig.py
+srun conda run -p /mnt/qb/work2/butz1/bst080/miniconda3/envs/train_env python /mnt/qb/work/butz/bst080/faceGANtts/run_orig.py
